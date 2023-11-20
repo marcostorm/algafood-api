1ee636a0590ec61b52459d2717907cd2f27cb007
marcos.araujo <marcos.araujo@tceac.tc.br>
2023-11-20 12:45:56 -0300

    Correção dos imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,7 @@
-insert into cozinha (nome)values(1, 'Tailandesa');
-insert into cozinha (nome)values(2, 'Indiana');
+insert into cozinha (id, nome) values (1, 'Tailandesa');
+insert into cozinha (id, nome) values (2, 'Indiana');
 
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
-insert into restaurante(nome, taxa_frete, cozinha_id) values('Thai Delivery', 9.50, 1);
-insert into restaurante(nome, taxa_frete, cozinha_id) values('Tuk Tuk Comida Indiana', 15, 2);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (1, 'Thai Gourmet', 10, 1);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 15, 2);
+
