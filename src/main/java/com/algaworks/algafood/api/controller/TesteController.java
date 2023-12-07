98a8e3bad6ca67fda854ac4c6bc75f8f825cd728
marcos.araujo <marcos.araujo@tceac.tc.br>
2023-12-07 14:41:11 -0300

    Usando keywords para definir critérios de query methods

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -2,11 +2,16 @@ package com.algaworks.algafood.api.controller;
 
 
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teste")
@@ -15,10 +20,27 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-  //  @GetMapping("/cozinhas/por-nome")
-  //  public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome){
-  //      return cozinhaRepository.consultarPorNome(nome);
-  //s  }
+    @Autowired
+    private RestauranteRepository restauranteRepository;
 
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(String nome){
+        return cozinhaRepository.findTodasByNomeContaining(nome);
+    }
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(String nome){
+        return cozinhaRepository.findByNome(nome);
+    }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restaurantePorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+    @GetMapping("/restaurantes/por-nome")
+    public List<Restaurante> restaurantePorTaxaFrete(String nome, Long cozinhaId) {
+        return restauranteRepository.findByNomeContainingAndId(nome, cozinhaId);
+    }
 
 }
