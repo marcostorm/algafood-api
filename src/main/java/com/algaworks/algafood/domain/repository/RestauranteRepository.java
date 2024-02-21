df0ac107d76c6a23e06f379c0a9ed713e8b57f13
marcos.araujo <marcos.araujo@tceac.tc.br>
2024-02-20 22:51:59 -0300

    Adicionando os testes com o callback do flyway

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -19,8 +19,9 @@ public interface RestauranteRepository
     // Errata: se um restaurante não tiver nenhuma forma de pagamento associada a ele,
     // esse restaurante não será retornado usando JOIN FETCH r.formasPagamento.
     // Para resolver isso, temos que usar LEFT JOIN FETCH r.formasPagamento
-//	@Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
-    @Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+    //	@Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
+    //@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+    @Query("from Restaurante r join fetch r.cozinha")
     List<Restaurante> findAll();
 
     List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
